152f94b933ae732bebfa3d208a7ba0fe6f5fb445
Nandini Bhattacharya <nandinib267@gmail.com>
2022-09-29 16:55:33 +0000

    Code from class on 22? Sep 2022

--- day4.c
+++ day4.c
@@ -13,21 +13,55 @@ int main(int argc, char **argv){
 	printf("%s\n",str);
 	printf("%c\n",*(str+1));
 	printf("----------------------\n");
-	const int n =5;
-	int *p=&n; //put location of n as value of p
+	const int m =5;
+	int *p=&m; //put location of n as value of p
 	*p=6; //put 6 in the location p (which we have said is the location of n
 	// intuitively, now n should be equal to 6 but n is a const
 	// is it changed?
-	printf("x = %d\n",n);
+	printf("x = %d\n",m);
 	printf("*p = %d\n",*p);
 	//it is changed. paap.
-	//computers are idiots
+	//computers are iiots
 	//and if you're spending so much to study them
 	//you're an idiot too
 	printf("----------------------\n");
-	const char *string = "Can't touch this";//at pos string put this phrase
+	const char *string = "Can't touch this";//at posofstring put this phrase
 	string = "Can't touch this either"; //let string be this phrase
 	printf("%s\n",string);
 	// printf("%s\n",*(string));  GIVES SEGMENTATION ERROR 
+	//even though cant touch this is inputted as a const at 
+	//whatever location string is at
+	//string can be given the value of cant touch this either
+	//idiot comp 
+	printf("----------------------\n");
+	int *a;
+	int i,n,val;
+	n = 3;
+	a = (int *) malloc(n*sizeof(int));
+	
+	//add data
+	i = 0;
+	scanf("%d",&val);
 
+	while (val>0){
+		a[i]=val;
+		i++;
+		if(i>=n){
+			n=n*2;
+			a = (int *) realloc(a,n*sizeof(int));
+		}
+		scanf("%d",&val); //what does this do?
+	}
+
+	//removing all excess memory alloc-ed to a
+	n=i;
+	a = realloc(a,n*sizeof(int)); 
+	printf("THe inputted data:\n");
+	//print data
+	for(i=0;i<n;i++){
+		printf("%d\n",a[i]);
+	}
+
+	printf("Done!\n");
+	return 0;
 }
